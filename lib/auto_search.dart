b775284b5e2d05787d670c21ba0e505921cbd5c1
Sudhanshu Bhagwat <sudhanshu_satishbhagwat@trimble.com>
2020-07-12 19:32:34 +0530

    Added a few more fields to interact freely with the TextEditingController

--- lib/auto_search.dart
+++ lib/auto_search.dart
@@ -13,6 +13,15 @@ class AutoSearchInput extends StatefulWidget {
   final Color cursorColor;
   final double borderRadius;
   final double fontSize;
+  final double singleItemHeight;
+  final int itemsShownAtStart;
+  final String hintText;
+  final bool autoCorrect;
+  final bool enabled;
+  final Function onSubmitted;
+  final Function onTap;
+  final Function onChanged;
+  final Function onEditingComplete;
 
   const AutoSearchInput({
     @required this.data,
@@ -25,6 +34,15 @@ class AutoSearchInput extends StatefulWidget {
     this.cursorColor,
     this.borderRadius = 10.0,
     this.fontSize = 20.0,
+    this.singleItemHeight = 40.0,
+    this.itemsShownAtStart = 10,
+    this.hintText = 'Enter a name',
+    this.autoCorrect = true,
+    this.enabled = true,
+    this.onSubmitted,
+    this.onTap,
+    this.onChanged,
+    this.onEditingComplete,
   }) : assert(data != null, maxElementsToDisplay != null);
 
   @override
@@ -56,39 +74,101 @@ class _AutoSearchInputState extends State<AutoSearchInput> {
     super.dispose();
   }
 
+  Widget _getRichText(String result) {
+    return RichText(
+      text: _textEditingController.text.length > 0
+          ? TextSpan(
+              children: [
+                if (_textEditingController.text.length > 0)
+                  TextSpan(
+                    text: result.substring(
+                        0,
+                        _textEditingController.text.indexOf(
+                              _textEditingController.text.substring(
+                                  _textEditingController.text.length - 1),
+                            ) +
+                            1),
+                    style: TextStyle(
+                      fontSize: widget.fontSize,
+                      color: widget.selectedTextColor != null
+                          ? widget.selectedTextColor
+                          : Colors.black,
+                    ),
+                  ),
+                TextSpan(
+                  text: result.substring(
+                    _textEditingController.text.indexOf(
+                          _textEditingController.text.substring(
+                              _textEditingController.text.length - 1),
+                        ) +
+                        1,
+                    result.length,
+                  ),
+                  style: TextStyle(
+                    fontSize: widget.fontSize,
+                    color: widget.unSelectedTextColor != null
+                        ? widget.unSelectedTextColor
+                        : Colors.grey[400],
+                  ),
+                )
+              ],
+            )
+          : TextSpan(
+              text: result,
+              style: TextStyle(
+                fontSize: widget.fontSize,
+                color: widget.unSelectedTextColor != null
+                    ? widget.unSelectedTextColor
+                    : Colors.grey[400],
+              ),
+            ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         TextField(
+          autocorrect: widget.autoCorrect,
+          enabled: widget.enabled,
+          onChanged: widget.onChanged,
+          onEditingComplete: widget.onEditingComplete,
+          onSubmitted: widget.onSubmitted,
+          onTap: widget.onTap,
           controller: _textEditingController,
           decoration: InputDecoration(
-            hintText: 'Enter a name',
+            hintText: widget.hintText,
             contentPadding: const EdgeInsets.all(10.0),
             disabledBorder: OutlineInputBorder(
               borderSide: BorderSide(
                   color: widget.disabledBorderColor != null
                       ? widget.borderRadius
                       : Colors.grey[200]),
-              borderRadius:
-                  BorderRadius.all(Radius.circular(widget.borderRadius)),
+              borderRadius: BorderRadius.all(
+                Radius.circular(widget.borderRadius),
+              ),
             ),
             enabledBorder: OutlineInputBorder(
               borderSide: BorderSide(
-                  color: widget.enabledBorderColor != null
-                      ? widget.borderRadius
-                      : Colors.grey[200]),
-              borderRadius:
-                  BorderRadius.all(Radius.circular(widget.borderRadius)),
+                color: widget.enabledBorderColor != null
+                    ? widget.borderRadius
+                    : Colors.grey[200],
+              ),
+              borderRadius: BorderRadius.all(
+                Radius.circular(widget.borderRadius),
+              ),
             ),
             focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                    color: widget.focusedBorderColor != null
-                        ? widget.borderRadius
-                        : Colors.grey[200]),
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(widget.borderRadius),
-                    topRight: Radius.circular(widget.borderRadius))),
+              borderSide: BorderSide(
+                  color: widget.focusedBorderColor != null
+                      ? widget.borderRadius
+                      : Colors.grey[200]),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(widget.borderRadius),
+                topRight: Radius.circular(widget.borderRadius),
+              ),
+            ),
           ),
           style: TextStyle(
             fontSize: widget.fontSize,
@@ -98,78 +178,28 @@ class _AutoSearchInputState extends State<AutoSearchInput> {
               : Colors.grey[600],
         ),
         Container(
-          height: 400,
+          height: widget.itemsShownAtStart * widget.singleItemHeight,
           child: ListView.builder(
             scrollDirection: Axis.vertical,
+            itemCount: results.length,
             itemBuilder: (context, index) {
               return Container(
-                height: 40,
+                height: widget.singleItemHeight,
                 padding: const EdgeInsets.all(8.0),
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.grey[300]),
                   borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(index == (results.length - 1)
-                        ? widget.borderRadius
-                        : 0.0),
-                    bottomRight: Radius.circular(index == (results.length - 1)
-                        ? widget.borderRadius
-                        : 0.0),
+                    bottomLeft: Radius.circular(
+                      index == (results.length - 1) ? widget.borderRadius : 0.0,
+                    ),
+                    bottomRight: Radius.circular(
+                      index == (results.length - 1) ? widget.borderRadius : 0.0,
+                    ),
                   ),
                 ),
-                child: RichText(
-                  text: _textEditingController.text.length > 0
-                      ? TextSpan(
-                          children: [
-                            if (_textEditingController.text.length > 0)
-                              TextSpan(
-                                text: results[index].substring(
-                                    0,
-                                    _textEditingController.text.indexOf(
-                                          _textEditingController.text.substring(
-                                              _textEditingController
-                                                      .text.length -
-                                                  1),
-                                        ) +
-                                        1),
-                                style: TextStyle(
-                                  fontSize: widget.fontSize,
-                                  color: widget.selectedTextColor != null
-                                      ? widget.selectedTextColor
-                                      : Colors.black,
-                                ),
-                              ),
-                            TextSpan(
-                              text: results[index].substring(
-                                _textEditingController.text.indexOf(
-                                      _textEditingController.text.substring(
-                                          _textEditingController.text.length -
-                                              1),
-                                    ) +
-                                    1,
-                                results[index].length,
-                              ),
-                              style: TextStyle(
-                                fontSize: widget.fontSize,
-                                color: widget.unSelectedTextColor != null
-                                    ? widget.unSelectedTextColor
-                                    : Colors.grey[400],
-                              ),
-                            )
-                          ],
-                        )
-                      : TextSpan(
-                          text: results[index],
-                          style: TextStyle(
-                            fontSize: widget.fontSize,
-                            color: widget.unSelectedTextColor != null
-                                ? widget.unSelectedTextColor
-                                : Colors.grey[400],
-                          ),
-                        ),
-                ),
+                child: _getRichText(results[index]),
               );
             },
-            itemCount: results.length,
           ),
         ),
       ],
